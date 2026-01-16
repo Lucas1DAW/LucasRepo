4133e6e4c272651824a4a6973e3a3eaf3745a8e8
Lucas <iesvda.lucrub@gmail.com>
2026-01-16 13:17:35 +0100

    prueba de push

---
+++ holamundo.java
@@ -0,0 +1,9 @@
+//Aplicaciones holamundo de ejemplo
+//
+
+class HolaMundoApp {
+	public static void main { string args[])
+	{
+	System.out.println ("Hola mundo!");
+	System.out.println ("Hola Git");
+}
